afc474fc859b8f43759bb2dadbed799575a0d353
sershishkov <ser.shishkov@gmail.com>
2019-08-12 20:30:05 +0300

    09-045

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,3 +8,6 @@ export const LOGOUT = 'LOGOUT';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
