5f62936a2e31126ef3d1ca8b2a385a37444cb688
sershishkov <ser.shishkov@gmail.com>
2019-08-13 19:54:47 +0300

    09-046

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -90,5 +91,6 @@ export const login = (email, password) => async dispatch => {
 
 //LOGout  / clear Profile
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
