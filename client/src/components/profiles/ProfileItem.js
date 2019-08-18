369469270a65bd9b23356aa105463830ba0349a3
sershishkov <ser.shishkov@gmail.com>
2019-08-18 17:32:37 +0300

    10-056

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -20,7 +20,9 @@ const ProfileItem = ({
           {status} {company && <span>at {company} </span>}{' '}
         </p>
         <p className='my-1'>{location && <span>{location} </span>} </p>
-        <Link to={`/profile/${_id}`}>View Profile</Link>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
+          View Profile
+        </Link>
       </div>
       <ul>
         {skills.slice(0, 4).map((skill, index) => (
