9ac14f2667d296e24be7814076fbb6b4dc43f6da
sershishkov <ser.shishkov@gmail.com>
2019-08-19 20:53:21 +0300

    11-060

---
+++ client/src/reducers/post.js
@@ -0,0 +1,28 @@
+import { GET_POST, POST_ERROR } from '../actions/types';
+
+const initialState = {
+  posts: [],
+  post: null,
+  loading: true,
+  error: {}
+};
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_POST:
+      return {
+        ...state,
+        posts: payload,
+        loading: false
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
